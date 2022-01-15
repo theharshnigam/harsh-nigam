343cf222f437f645711c6098537cc212342db667
Harsh Nigam <er.harshnigam@gmail.com>
2022-01-15 20:17:34 +0530

    added about content

--- src/components/AboutMe/About.js
+++ src/components/AboutMe/About.js
@@ -11,8 +11,13 @@ const About = () => {
                        <img src={aboutImg} alt="" className="img-fluid about__img" />
                     </div>
                     <div className='col-6'>
-                        <h1 className='about__heading'>About Me</h1>
-                        <h3>Hey, I am Harsh Nigam</h3>
+                        <div className='right_side'>
+                        <h1 className='about__heading'>Who am I ?</h1>
+                        <h3><span style={{color:"red"}}>Hey,</span> I am <span className='name'><strong>Harsh Nigam</strong></span></h3>
+                        <p>I am an engineering student & pursuing B.Tech in Computer Scinece Engineering from
+                            Ajay Kumar Garg Engineering College, Ghaziabad.
+                        </p>
+                        </div>
                     </div>
                 </div>
             </div>
