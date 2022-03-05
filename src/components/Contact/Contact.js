05c3b2fc3c6390eeedd78cb52aa079376c1f5718
Harsh Nigam <er.harshnigam@gmail.com>
2022-03-05 17:03:32 +0530

    change2

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,21 +7,23 @@ import '../Contact/Contact.css'
 const Contact = () => {
   return (
     <div className='contact_main' id='Contact'>
-        <div>
-            <h1 style={{color:"orange", textAlign:"center"}}>Get in touch with...</h1>
-         <div className='contact_content'>
-            <a href='https://www.linkedin.com/in/imharshnigam' target="_blank"><img className='img-fluid logo' src={linkedin}/></a>
-            <h3>Linked In</h3> 
+      <div>
+        <div className='title_heading'>
+          <h1 >Get in touch with...</h1>
         </div>
         <div className='contact_content'>
-            <a href='https://www.instagram.com/theharshnigam' target="_blank"><img className='img-fluid logo' src={instagram}/></a>
-            <h3>Instagram</h3> 
+          <a href='https://www.linkedin.com/in/imharshnigam' target="_blank"><img className='img-fluid logo' src={linkedin} /></a>
+          <h3>Linked In</h3>
         </div>
         <div className='contact_content'>
-            <a href='https://www.facebook.com/iamharshnigam' target="_blank"><img className='img-fluid logo' src={facebook}/></a>
-            <h3>Facebook</h3> 
+          <a href='https://www.instagram.com/theharshnigam' target="_blank"><img className='img-fluid logo' src={instagram} /></a>
+          <h3>Instagram</h3>
         </div>
+        <div className='contact_content'>
+          <a href='https://www.facebook.com/iamharshnigam' target="_blank"><img className='img-fluid logo' src={facebook} /></a>
+          <h3>Facebook</h3>
         </div>
+      </div>
     </div>
   )
 }
