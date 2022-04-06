f7b444b74addfa8063846a86db137c14fce911f8
Harsh Nigam <er.harshnigam@gmail.com>
2022-04-07 02:01:33 +0530

    added projects card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,37 +1,59 @@
 import React from 'react'
 import '../Projects/Projects.css'
-import project1 from '../../images/Projects/project1.png'
+import p1 from '../../images/Projects/p1.jpg'
+import p2 from '../../images/Projects/p2.jpg'
+import p3 from '../../images/Projects/p3.jpg'
 
 const Projects = () => {
-  return (
-    <div className='project_main' id='Projects'>
-      <div className='container'>
-      <div className='title_heading'>
-        <h1 >Projects</h1>
-      </div>
-      <div className='project1'>
-        <div className='row'>
-          <div className='col-sm-6'>
-            <h2 className='heading'>Web Chat Application</h2>
-            <p className='content'>In this web app, a user is required to authenticate using Google or Facebook.
-              After successful authentication, the user will be redirected to the homepage of the chat app where the user
-              can see two options. First to interact with his profile management and second is to create room for messaging.
-              Message can be text, image or any valid file and user can also send live recorded voice on the web app.
-              <br />
-            </p>
-            <div className='BTNprops'>
-              <a href='https://chat-app-5ed3c.web.app' target="_blank">
-                <p><button className='ViewProjectBTN'>View Project</button></p>
-              </a>
-            </div>
-          </div>
-          <div className='col-sm-6 projectPicSide'>
-            <img className='project1_img' src={project1} />
-          </div>
-        </div>
-      </div>
+  return(
+    <div className="project_main">
+    <div className="container">
+    <div className='title_heading'>  
+        <h1>Projects</h1>
+    </div>
+    <div className="card-section">
+    <div className="card mb-3" style={{maxWidth: "540px"}}>
+  <div className="row g-1 ">
+    <div className="col-md-6">
+      <div className="project_img">
+      <img src={p1} className="img-fluid  " alt="..."/>
       </div>
     </div>
+    <div className="col-md-6">
+      <div className="card-body">
+        <h5 className="card-title">Web Chat Application</h5>
+        <p className="card-text">This is a real time chat application in which we can communicate using text, voice or supporting attachment media.</p>
+        <p className="card-text"><a href="https://chat-app-5ed3c.web.app" target="_blank" class="btn btn-primary">View Project</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div className="card-section">
+    <div className="card mb-3" style={{maxWidth: "540px"}}>
+  <div className="row g-1 ">
+    <div className="col-md-6">
+      <div className="project_img">
+      <img src={p2} className="img-fluid  " alt="..."/>
+      </div>
+    </div>
+    <div className="col-md-6">
+      <div className="card-body">
+        <h5 className="card-title">Tic-Tac-Toe Game</h5>
+        <p className="card-text">Tic-tac-toe is a game in which two players take turns in drawing either an ' O' or an ' X' in one square of a grid consisting of nine squares. The winner is the first player to get three of the same symbols in a row.</p>
+        <p className="card-text"><a href="https://theharshnigam.github.io/tic-tac-toe/" target="_blank" class="btn btn-primary">View Project</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+    
+    </div>
+</div>
   )
 }
 
