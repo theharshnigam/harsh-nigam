11d4d4ce6631b236413e8fcaf4f2420d1f470e33
Harsh Nigam <er.harshnigam@gmail.com>
2022-05-26 01:15:43 +0530

    Changing Contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,17 +9,23 @@ const Contact = () => {
     <div className='contact_main' id='Contact'>
       <div className='container'>
         <div className='title_heading'>
-          <h1 >Contacts</h1>
+          <h1 >Get in Touch</h1>
         </div>
-        <div className="row justify-content-end">
-          <div className='col-3'>
-            <a href='https://www.linkedin.com/in/imharshnigam' target="_blank" rel="noreferrer" ><img className='img-fluid' src={linkedin} /></a>
+        <div className="d-flex justify-content-center" >
+          <div className='contact_link' title="Linkedin">
+            <a href='https://www.linkedin.com/in/imharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-linkedin"></i></a>
           </div>
-          <div className='col-3 '>
-            <a href='https://www.instagram.com/theharshnigam' target="_blank" rel="noreferrer" ><img className='img-fluid' src={instagram} /></a>
+          <div className='contact_link ' title="Instagram">
+            <a href='https://www.instagram.com/theharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-instagram-square"></i></a>
           </div>
-          <div className='col-3'>
-            <a href='https://www.facebook.com/iamharshnigam' target="_blank" rel="noreferrer" ><img className='img-fluid' src={facebook} /></a>
+          <div className='contact_link' title="Facebook">
+            <a href='https://www.facebook.com/iamharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-facebook-square"></i></a>
+          </div>
+          <div className='contact_link' title="Github">
+            <a href='https://www.github.com/theharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-github-square"></i></a>
+          </div>
+          <div className='contact_link' title="Email">
+            <a href='mailto:er.harshnigam@gmail.com' target="_blank" rel="noreferrer" ><i class="fa-solid fa-square-envelope"></i></a>
           </div>
         </div>
       </div>
