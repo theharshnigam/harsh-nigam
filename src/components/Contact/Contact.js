1cb98cbfe88a342514249a3f17c4b0a53b549238
Harsh Nigam <er.harshnigam@gmail.com>
2022-05-26 16:36:38 +0530

    AOS effect on contacts

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import instagram from '../../images/insta.png'
-import facebook from '../../images/fb.png'
-import linkedin from '../../images/linkedin.png'
 import '../Contact/Contact.css'
 
 const Contact = () => {
@@ -12,20 +9,20 @@ const Contact = () => {
           <h1 >Get in Touch</h1>
         </div>
         <div className="d-flex justify-content-center" >
-          <div className='contact_link' title="Linkedin">
+          <div className='contact_link' title="Linkedin" data-aos="fade-up" data-aos-duration="1800">
             <a href='https://www.linkedin.com/in/imharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-linkedin"></i></a>
           </div>
-          <div className='contact_link ' title="Instagram">
+          <div className='contact_link ' title="Instagram" data-aos="fade-up" data-aos-duration="1600">
             <a href='https://www.instagram.com/theharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-instagram-square"></i></a>
           </div>
-          <div className='contact_link' title="Facebook">
+          <div className='contact_link' title="Facebook" data-aos="fade-up" data-aos-duration="1400">
             <a href='https://www.facebook.com/iamharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-facebook-square"></i></a>
           </div>
-          <div className='contact_link' title="Github">
+          <div className='contact_link' title="Github" data-aos="fade-up" data-aos-duration="1200">
             <a href='https://www.github.com/theharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-github-square"></i></a>
           </div>
-          <div className='contact_link' title="Email">
-            <a href='mailto:er.harshnigam@gmail.com' target="_blank" rel="noreferrer" ><i class="fa-solid fa-square-envelope"></i></a>
+          <div className='contact_link' title="Email" data-aos="fade-up" data-aos-duration="1000">
+            <a href='mailto:iharshnigam@gmail.com' target="_blank" rel="noreferrer" ><i class="fa-solid fa-square-envelope"></i></a>
           </div>
         </div>
       </div>
