80523c2b1215da8d510f3278b51b01a21ce8f5ff
Harsh Nigam <er.harshnigam@gmail.com>
2022-05-26 16:55:31 +0530

    AOS effect on contacts

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,19 +9,19 @@ const Contact = () => {
           <h1 >Get in Touch</h1>
         </div>
         <div className="d-flex justify-content-center" >
-          <div className='contact_link' title="Linkedin" data-aos="fade-right" data-aos-duration="1800">
+          <div className='contact_link' title="Linkedin">
             <a href='https://www.linkedin.com/in/imharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-linkedin"></i></a>
           </div>
-          <div className='contact_link ' title="Instagram" data-aos="fade-right" data-aos-duration="1600">
+          <div className='contact_link ' title="Instagram">
             <a href='https://www.instagram.com/theharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-instagram-square"></i></a>
           </div>
-          <div className='contact_link' title="Facebook" data-aos="fade-right" data-aos-duration="1400">
+          <div className='contact_link' title="Facebook">
             <a href='https://www.facebook.com/iamharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-facebook-square"></i></a>
           </div>
-          <div className='contact_link' title="Github" data-aos="fade-right" data-aos-duration="1200">
+          <div className='contact_link' title="Github">
             <a href='https://www.github.com/theharshnigam' target="_blank" rel="noreferrer" ><i class="fa-brands fa-github-square"></i></a>
           </div>
-          <div className='contact_link' title="Email" data-aos="fade-right" data-aos-duration="1000">
+          <div className='contact_link' title="Email">
             <a href='mailto:iharshnigam@gmail.com' target="_blank" rel="noreferrer" ><i class="fa-solid fa-square-envelope"></i></a>
           </div>
         </div>
