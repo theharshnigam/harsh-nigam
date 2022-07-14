01269f6d9964dd1e9f924505482fb66d26c8a3fd
Harsh Nigam <er.harshnigam@gmail.com>
2022-07-14 12:13:23 +0530

    Updates

---
+++ precache-manifest.5be660fd175e4308ff1269175b68b68d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bb75faa7bc1bc575cdc",
+    "url": "/portfolio/static/css/main.9d79347d.chunk.css"
+  },
+  {
+    "revision": "1bb75faa7bc1bc575cdc",
+    "url": "/portfolio/static/js/main.1bb75faa.chunk.js"
+  },
+  {
+    "revision": "fa2b38315574cd228106",
+    "url": "/portfolio/static/css/1.f7ff3636.chunk.css"
+  },
+  {
+    "revision": "fa2b38315574cd228106",
+    "url": "/portfolio/static/js/1.fa2b3831.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "83b74fdcab526c09acd8a702cc707e7e",
+    "url": "/portfolio/static/media/mypic.83b74fdc.png"
+  },
+  {
+    "revision": "8cb5ca91fdb3a8ec3accf05e033fed88",
+    "url": "/portfolio/static/media/Harsh_CV.8cb5ca91.pdf"
+  },
+  {
+    "revision": "64a40ed9d66834968cbdfb4b8f893509",
+    "url": "/portfolio/static/media/Harsh_cv.64a40ed9.jpg"
+  },
+  {
+    "revision": "9fb1f77d8219f7b54fa81307f28cf270",
+    "url": "/portfolio/static/media/pp1.9fb1f77d.png"
+  },
+  {
+    "revision": "d0ec0fe3ab4bfe37977d56fa323507d3",
+    "url": "/portfolio/static/media/p2.d0ec0fe3.jpg"
+  },
+  {
+    "revision": "8cdf6573921255a4bf50fe5e64412fe7",
+    "url": "/portfolio/static/media/p3.8cdf6573.jpg"
+  },
+  {
+    "revision": "89777a9a42600ef35d2b9725426e2ba9",
+    "url": "/portfolio/static/media/background.89777a9a.jpg"
+  },
+  {
+    "revision": "45e94473322967915dcb09c064853782",
+    "url": "/portfolio/static/media/footer.45e94473.png"
+  },
+  {
+    "revision": "8b51a3bf77f6f81700fb87ed5752e691",
+    "url": "/portfolio/index.html"
+  }
+];
