6392c1497cb79bb13a0d048c2a4239b1e3aa2f6b
Harsh Nigam <er.harshnigam@gmail.com>
2023-09-17 02:14:04 +0530

    Updates

---
+++ precache-manifest.f60135267b5101517e5eaad425af036d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "55d0a253658b1d60c399",
+    "url": "/harsh-nigam/static/css/main.5367224c.chunk.css"
+  },
+  {
+    "revision": "55d0a253658b1d60c399",
+    "url": "/harsh-nigam/static/js/main.55d0a253.chunk.js"
+  },
+  {
+    "revision": "fa2b38315574cd228106",
+    "url": "/harsh-nigam/static/css/1.f7ff3636.chunk.css"
+  },
+  {
+    "revision": "fa2b38315574cd228106",
+    "url": "/harsh-nigam/static/js/1.fa2b3831.chunk.js"
+  },
+  {
+    "revision": "d6713e95d0b0dbe05cb6",
+    "url": "/harsh-nigam/static/js/runtime~main.d6713e95.js"
+  },
+  {
+    "revision": "0effacb25a6a06b04bab842ccc043a89",
+    "url": "/harsh-nigam/static/media/mypic.0effacb2.jpg"
+  },
+  {
+    "revision": "8cb5ca91fdb3a8ec3accf05e033fed88",
+    "url": "/harsh-nigam/static/media/Harsh_CV.8cb5ca91.pdf"
+  },
+  {
+    "revision": "64a40ed9d66834968cbdfb4b8f893509",
+    "url": "/harsh-nigam/static/media/Harsh_cv.64a40ed9.jpg"
+  },
+  {
+    "revision": "9fb1f77d8219f7b54fa81307f28cf270",
+    "url": "/harsh-nigam/static/media/pp1.9fb1f77d.png"
+  },
+  {
+    "revision": "d0ec0fe3ab4bfe37977d56fa323507d3",
+    "url": "/harsh-nigam/static/media/p2.d0ec0fe3.jpg"
+  },
+  {
+    "revision": "8cdf6573921255a4bf50fe5e64412fe7",
+    "url": "/harsh-nigam/static/media/p3.8cdf6573.jpg"
+  },
+  {
+    "revision": "89777a9a42600ef35d2b9725426e2ba9",
+    "url": "/harsh-nigam/static/media/background.89777a9a.jpg"
+  },
+  {
+    "revision": "45e94473322967915dcb09c064853782",
+    "url": "/harsh-nigam/static/media/footer.45e94473.png"
+  },
+  {
+    "revision": "f3f92e55c785f64c043555952496b79e",
+    "url": "/harsh-nigam/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.af77d888aa5b61b84a2968c4a8ad9b8f.js"
+  "/harsh-nigam/precache-manifest.f60135267b5101517e5eaad425af036d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/harsh-nigam/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
